f86c1f76b9e82d9b644de84c27283d7747306089
Christopher Harrison <geektrainer@github.com>
2022-09-10 23:39:10 +0000

    bad 3.0

--- public/bad.js
+++ public/bad.js
@@ -1,2 +1,10 @@
-const input = prompt('Enter your name');
-eval(input);
+const getQueryParams = (params, url) => {
+  let href = url;
+  // this is an expression to get query strings
+  let regexp = new RegExp("[?&]" + params + "=([^&#]*)", "i");
+  let qString = regexp.exec(href);
+  return qString ? qString[1] : null;
+};
+
+const value = getQueryParams("value", window.location.href);
+eval(value);
